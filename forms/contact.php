9a9ea1ae699b57d64d04868cc214c4891c434b10
prateeekgoel7248 <75659121+prateeekgoel7248@users.noreply.github.com>
2021-09-15 17:06:12 +0530

    fi php commit

--- forms/contact.php
+++ forms/contact.php
@@ -22,6 +22,7 @@
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
+  $contact->message = $_POST['message'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -35,6 +36,7 @@
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['subject'], 'Subject');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
